ef0105e526fa33eae032c2e2ace4e99af50bd480
Lei Deng <ldeng.sjtu@gmail.com>
2026-03-23 18:08:52 +0800

    Remove duplicate tokenizer initialization

--- scripts/data_process/fineweb.py
+++ scripts/data_process/fineweb.py
@@ -36,7 +36,6 @@ def set_args():
 
 def main(argv):
     tokenizer = AutoTokenizer.from_pretrained("alpindale/Llama-3.2-1B-Instruct")
-    tokenizer = AutoTokenizer.from_pretrained("alpindale/Llama-3.2-1B-Instruct")
     num_samples = FLAGS.num_samples
     dataset = load_dataset(
         "HuggingFaceFW/fineweb",
